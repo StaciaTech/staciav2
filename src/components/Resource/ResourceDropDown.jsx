945595826f2e960422a47095e6f6b407dbd9d62f
007moses <moses05112000@gmail.com>
2025-04-09 12:05:47 +0530

    Updated About, Resources, WhatsNew nav Dropdown components as per bug report

--- src/components/Resource/ResourceDropDown.jsx
+++ src/components/Resource/ResourceDropDown.jsx
@@ -470,7 +470,7 @@ function ResourceDropDown({ handleClose }) {
       const firstDept = articlesData[0];
       const firstArticle = firstDept?.data?.[0];
 
-      setActiveArt(defaultRes);
+      setActiveRes(defaultRes);
       setActiveDept(firstDept?.name);
       if(firstArticle){
         setActiveArt(firstArticle?.title);
@@ -533,7 +533,16 @@ function ResourceDropDown({ handleClose }) {
           return (
             <div
               key={i}
-              onMouseEnter={() => setActiveRes(eachRes.name)}
+              onMouseEnter={() => {
+                setActiveRes(eachRes.name)
+              
+                const firstDept = eachRes?.cats?.[0];
+                const firstItem = firstDept?.data?.[0];
+
+                setActiveDept(firstDept?.name|| null);
+                setActiveArt(firstItem?.title || null);
+                setFoundItem(firstItem)
+              }}
               className={`res-main-item pointer ${eachRes.name === activeRes ? "res-main-item-active" : ""
                 }`}
               onClick={() => {
@@ -567,7 +576,13 @@ function ResourceDropDown({ handleClose }) {
               return (
                 <div
                   key={i}
-                  onMouseEnter={() => setActiveDept(eachDept.name)}
+                  onMouseEnter={() => {
+                    setActiveDept(eachDept.name)
+                  
+                  const firstItem = eachDept?.data?.[0];
+                  setActiveArt(firstItem?.title || null);
+                  setFoundItem(firstItem)
+                  }}
                   className={`res-main-item pointer ${eachDept.name === activeDept ? "res-main-item-active" : ""
                     }`}
                   onClick={() => {
