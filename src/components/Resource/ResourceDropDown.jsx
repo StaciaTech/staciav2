904aee288b7c949589cdbe162f018857d257787a
Thilak Chandran <thilakct28@gmail.com>
2025-04-15 18:00:22 +0530

    partner page and about dropdown

--- src/components/Resource/ResourceDropDown.jsx
+++ src/components/Resource/ResourceDropDown.jsx
@@ -667,25 +667,47 @@ function ResourceDropDown({ handleClose }) {
       {foundItem && activeArt && (
         <div className="res-item-contaienr">
           <div>
-            <div className="res-item-card-image">
+            <div className="res-item-card-image"
+            onClick={() => {
+              window.scrollTo(0, 0);
+              if (activeRes === "Case Study") {
+                // Navigate using ID
+                navigate(`/case-study/single-caseStudy/${foundItem?.id || ""}`);
+              } else {
+                // Navigate using title
+                const resSlug =
+                  activeRes?.toLowerCase().replace(/\s+/g, "-") || "";
+                const deptSlug = activeDept?.replace(/\s+/g, "") || "";
+                const titleSlug = foundItem?.title?.replace(/\s+/g, "-") || "";
+                navigate(`/${resSlug}/${deptSlug}/${titleSlug}`);
+              }
+              handleClose();
+            }}
+            style={{cursor:"pointer"}}>
               <img src={foundItem?.imageURL || foundItem?.mainImageUrl} alt="" />
             </div>
             <div className="res-item-card-title" >{foundItem?.title}</div>
             <p className="res-item-card-des">{foundItem?.description}</p>
           </div>
           <div
-            style={{ display: "flex", justifyContent: "end", width: "100%" }}
+            style={{ display: "flex", justifyContent: "end", width: "100%" ,cursor:"pointer"}}
             className="know-more"
             onClick={() => {
               window.scrollTo(0, 0);
-              navigate(`/case-study/single-caseStudy/${foundItem?.id || ""}`); // path casestudy
-              handleClose();
-              navigate(
-                `/${activeRes?.toLowerCase().replace(/\s+/g, "-") || ""}/${activeDept?.replace(/\s+/g, "") || ""
-                }/${foundItem?.title?.replace(/\s+/g, "-") || ""}`
-              );
+              if (activeRes === "Case Study") {
+                // Navigate using ID
+                navigate(`/case-study/single-caseStudy/${foundItem?.id || ""}`);
+              } else {
+                // Navigate using title
+                const resSlug =
+                  activeRes?.toLowerCase().replace(/\s+/g, "-") || "";
+                const deptSlug = activeDept?.replace(/\s+/g, "") || "";
+                const titleSlug = foundItem?.title?.replace(/\s+/g, "-") || "";
+                navigate(`/${resSlug}/${deptSlug}/${titleSlug}`);
+              }
               handleClose();
             }}
+          
           >
             Know More
           </div>
