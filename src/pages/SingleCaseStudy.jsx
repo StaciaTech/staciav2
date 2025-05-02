c90d4bb80cd18db5245982ea5b4fceee900e3197
007moses <moses05112000@gmail.com>
2025-05-02 16:26:47 +0530

    Dot container in Products page and content updated in projects page.

--- src/pages/SingleCaseStudy.jsx
+++ src/pages/SingleCaseStudy.jsx
@@ -560,7 +560,7 @@ function SingleCaseStudy() {
       const windowHeight = window.innerHeight;
       const documentHeight = document.documentElement.scrollHeight;
       const bottomOffset = 1000; // Adjust this value as needed
-      if (scrollY > 1500) {
+      if (scrollY > 4200) {
         if (scrollY + windowHeight >= documentHeight - bottomOffset) {
           setShowButton(false); // Hide when near bottom
         } else {
