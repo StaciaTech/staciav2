58544bd31ef50fa0d635141a66a07a89976e21f7
epraveenkumar7 <ipraveen.e@gmail.com>
2025-05-03 17:07:29 +0530

    Fixed minor bug FourCard component and Four.css styles.

--- src/components/Home/FourCard.jsx
+++ src/components/Home/FourCard.jsx
@@ -4,7 +4,16 @@ import { IoIosArrowForward } from "react-icons/io";
 import { FaEnvelope, FaLinkedinIn } from "react-icons/fa";
 export default function FourCard({ data, path }) {
   const navigate = useNavigate();
-
+  const animateText = (text) =>
+    text.split("").map((char, idx) => (
+      <span
+        key={idx}
+        className="char"
+        style={{ transitionDelay: `${idx * 50}ms` }}
+      >
+        {char}
+      </span>
+    ));
   return (
     <div className="four-card">
       <div className="four-pos test-seclection-blue">{data.pos}</div>
@@ -21,7 +30,7 @@ export default function FourCard({ data, path }) {
           {/* LinkedIn Icon */}
           <div className="icon-wrapper">
             <a href={data.proId} target="_blank" rel="noopener noreferrer">
-              <div className="leader-icon-container" >
+              <div className="leader-icon-container">
                 <FaLinkedinIn className="footer-linkedin-icon" />
               </div>
               <div className="icon-name">LinkedIn</div>
@@ -38,7 +47,7 @@ export default function FourCard({ data, path }) {
               <div className="leader-icon-container">
                 <FaEnvelope className="footer-facebook-icon" />
               </div>
-              <div className="icon-name" > Email</div>
+              <div className="icon-name"> Email</div>
             </a>
           </div>
         </div>
@@ -50,15 +59,3 @@ export default function FourCard({ data, path }) {
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
