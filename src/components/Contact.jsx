2fa67ecefee6450b8544ac51673695dfbd26f1cf
Thilak Chandran <thilakct28@gmail.com>
2025-06-02 11:28:57 +0530

    Contact.css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -567,7 +567,7 @@ function Contact({ closeHandle }) {
             <div>
               <div style={{ display: "flex", justifyContent: "space-between" }}>
                 <div className="contact-main-title">
-                  Love to hear from you :blue_heart:
+                  Love to hear from you 💙
                 </div>
                 <IoClose
                   onClick={closeHandle}
