98c1167bb7515fa5f08072faef6e4677553125d1
thilak <thilakct28@gmail.com>
2025-11-17 10:53:59 +0530

    access key  change in contact us

--- src/components/LeadModalThree.jsx
+++ src/components/LeadModalThree.jsx
@@ -1216,7 +1216,7 @@ const LeadModalThree = ({ isOpen, onClose, item, onSubmit }) => {
     }
 
     const submissionData = new FormData();
-    submissionData.append("access_key", "31997e7a-b269-417f-8543-e6c4adc63ad9");
+    submissionData.append("access_key", "f05920d0-3b2a-427b-bd0e-de098dfadd58");
     submissionData.append("name", formData.name);
     submissionData.append("email", formData.email);
     submissionData.append("phone", getCleanPhoneNumber(formData.phone));
