7ada4706d780628c2d05083547fae2a978254d7c
thilak <thilakct28@gmail.com>
2025-11-27 15:40:23 +0530

    changes log
    -Implement NewsRoomHome component with carousel functionality and styles in home page
    
    - access key are changes in careers and contactus components

---
+++ src/components/ReUsableComp/Others.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/Others.css';
+
+const Others = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Others;
